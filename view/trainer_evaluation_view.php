30fe1b014cda50a36a842afaf36adc2a8ba18d93
Rajan Lad <rajanrlad71293@gmail.com>
2019-07-06 21:55:01 +0200

    Update trainer_evaluation_view.php
    Quiz List (Title of quiz)

--- view/trainer_evaluation_view.php
+++ view/trainer_evaluation_view.php
@@ -23,5 +23,34 @@
   <ul>
 </div>
 
+    <div class="quizID-list">
+  <h1> Quiz List </h1>
+
+  <ul>
+  <?php
+    if (count($getQuiz) > 0) {
+      foreach ($getQuiz as $uneQuiz) {
+        $trainer_id=$uneQuiz["creator_id"];
+        $evaluation_id = $uneQuiz["evaluation_id"];
+        $quiz_id = $uneQuiz["quiz_id"];
+        $title = $uneQuiz["title"];
+        ?>
+        <div class="quiz-card">
+
+        <!-- Based on sequence on Diagram -->
+        <!-- /trainer-{trainer-id}-evaluations-{evaluation-id}-quiz-{quiz-id} -->
+
+        <? if($correctedVal == NULL){ ?>
+          <li>Quiz ID: <a href=<?= 'trainer-' . $trainer_id . '-evaluations-'.$evaluation_id.'-quiz-'.$quiz_id ?>><?=$title?></a></li><br>          
+        </div>
+        <?php
+      }
+    } else {
+      echo "Any evaluation is coming";
+    }
+    ?>
+  <ul>
+</div>
+    
 <?php require_once ('footer.php'); ?>
 
