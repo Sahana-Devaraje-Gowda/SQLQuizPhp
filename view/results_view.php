1c5744ae3e1d4601897aba7400e6dcd1c3498611
Rajan Lad <rajanrlad71293@gmail.com>
2019-07-08 15:39:43 +0200

    Update results_view.php

--- view/results_view.php
+++ view/results_view.php
@@ -1,1 +1,35 @@
 
+<?php require_once("header.php"); ?>
+<div class="results-list">
+  <h1> Results</h1>
+
+  <ul>
+
+  <table style="width:100%">
+  <?php
+    if (count($getResults) > 0) {
+      foreach ($getResults as $uneResults) {
+        $question_text = $uneResults["question_text"];
+        $answer = $uneResults["answer"];
+        $result = $uneResults["result"];
+        $correct_answer = $uneResults["correct_answer"];
+        $correct_result = $uneResults["correct_result"];
+    
+        ?>
+          <tr>
+          <th><?=$question_text?></th>
+          <th><?=$answer?></th> 
+          <th><?=$result?></th>
+          <th><?=$correct_answer?></th>
+          <th><?=$correct_result?></th>
+          
+        </tr>
+        <?php
+        }
+    } 
+    ?>
+    </table>
+  <ul>
+</div>
+<?php require_once ('footer.php'); ?>
+
